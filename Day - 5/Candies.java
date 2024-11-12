b34baa7360a0e49dc1098e9b812dbf567d267899
Lakshay Saxena <163431247+iCodeLakshay@users.noreply.github.com>
2024-11-12 16:37:47 +0000

    main

---
+++ Day - 5/Candies.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Result {
+    public static long candies(int n, List<Integer> arr) {
+        if (n == 0) return 0;
+
+        int[] candies = new int[n];
+        
+        Arrays.fill(candies, 1);
+        
+        for (int i = 1; i < n; i++) {
+            if (arr.get(i) > arr.get(i - 1)) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr.get(i) > arr.get(i + 1)) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        
+        long totalCandies = 0;
+        for (int candy : candies) {
+            totalCandies += candy;
+        }
+        
+        return totalCandies;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = IntStream.range(0, n).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
+
+        long result = Result.candies(n, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
